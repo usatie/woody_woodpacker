4ff094085e6dcfedc8e5b5fb9ad333798fbbf333
Shun Usami <usatie@gmail.com>
2025-01-12 16:55:20 +0900

    Fix the format specifiers for the ELF header fields

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h> // exit
+#include <stdint.h>
 
 void usage_error() {
 	dprintf(STDERR_FILENO, "usage: ./woody_woodpacker filename\n");
@@ -25,6 +26,29 @@ typedef struct ELFHeader {
 	uint16_t e_shstrndx;
 } ELFHeader;
 
+void print_elf_header(ELFHeader *h) {
+	printf("e_ident:     ");
+	for (int i = 0; i < 16; ++i) {
+		printf("%02x", h->e_ident[i]);
+		if (i != 15) printf(" ");
+		if (i == 7) printf(" ");
+	}
+	printf("\n");
+	printf("e_type:      0x%x\n", h->e_type);
+	printf("e_machine:   0x%x\n", h->e_machine);
+	printf("e_version:   0x%x\n", h->e_version);
+	printf("e_entry:     0x%lx\n", h->e_entry);
+	printf("e_phoff:     0x%lx\n", h->e_phoff);
+	printf("e_shoff:     0x%lx\n", h->e_shoff);
+	printf("e_flags:     0x%x\n", h->e_flags);
+	printf("e_ehsize:    0x%x\n", h->e_ehsize);
+	printf("e_phentsize: 0x%x\n", h->e_phentsize);
+	printf("e_phnum:     0x%x\n", h->e_phnum);
+	printf("e_shentsize: 0x%x\n", h->e_shentsize);
+	printf("e_shnum:     0x%x\n", h->e_shnum);
+	printf("e_shstrndx:  0x%x\n", h->e_shstrndx);
+}
+
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
 		usage_error();
@@ -39,26 +63,7 @@ int main(int argc, char *argv[]) {
 	int rc = read(fd, buf, sizeof(ELFHeader));
 	printf("rc = %d\n", rc);
 	ELFHeader *h = (ELFHeader *)buf;
-	printf("e_ident:     ");
-	for (int i = 0; i < 16; ++i) {
-		printf("%02x", h->e_ident[i]);
-		if (i != 15) printf(" ");
-		if (i == 7) printf(" ");
-	}
-	printf("\n");
-	printf("e_type:      0x%x\n", h->e_type);
-	printf("e_machine:   0x%x\n", h->e_machine);
-	printf("e_version:   0x%x\n", h->e_version);
-	printf("e_entry:     0x%llx\n", h->e_entry);
-	printf("e_phoff:     0x%llx\n", h->e_phoff);
-	printf("e_shoff:     0x%llx\n", h->e_shoff);
-	printf("e_flags:     0x%x\n", h->e_flags);
-	printf("e_ehsize:    0x%x\n", h->e_ehsize);
-	printf("e_phentsize: 0x%x\n", h->e_phentsize);
-	printf("e_phnum:     0x%x\n", h->e_phnum);
-	printf("e_shentsize: 0x%x\n", h->e_shentsize);
-	printf("e_shnum:     0x%x\n", h->e_shnum);
-	printf("e_shstrndx:  0x%x\n", h->e_shstrndx);
+	print_elf_header(h);
 	close(fd);
 	return 0;
 }
